3568e0638ea948b6cf1c39dedd703ff3e56b41c3
JKrishnaD <jayak5063@gmail.com>
2025-08-27 23:50:50 +0530

    added redis publish channels

--- crates/redis-adapter/src/lib.rs
+++ crates/redis-adapter/src/lib.rs
@@ -1,14 +1,36 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use anyhow::Result;
+use redis::Client;
+
+// A trait for publishing messages to a channel
+#[async_trait::async_trait]
+pub trait Publisher: Send + Sync {
+    fn publisher(&self, channel: &str, payload: &[u8]) -> Result<(), anyhow::Error>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct RedisPublisher {
+    pub client: Client,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl RedisPublisher {
+    pub fn new(rpc_url: &str) -> Result<Self> {
+        let client = Client::open(rpc_url)?;
+        Ok(Self { client })
+    }
+}
+
+#[async_trait::async_trait]
+impl Publisher for RedisPublisher {
+    fn publisher(&self, channel: &str, payload: &[u8]) -> Result<(), anyhow::Error> {
+        // getting a client connection
+        let mut connection = self.client.get_connection()?;
+        // converting the bytes which we get from the geyser into string
+        let payload_str = String::from_utf8(payload.to_vec());
+
+        // publishing the message which is the data to the respective channel
+        redis::cmd("PUBLISH")
+            .arg(channel)
+            .arg(payload_str.unwrap())
+            .exec(&mut connection)?;
+        Ok(())
     }
 }
