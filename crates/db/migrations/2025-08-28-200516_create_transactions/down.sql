2b76a83f50336787b6584355f8f0dbfebae26c9a
JKrishnaD <jayak5063@gmail.com>
2025-08-29 12:11:40 +0530

    changed tables schema

--- crates/db/migrations/2025-08-28-200516_create_transactions/down.sql
+++ crates/db/migrations/2025-08-28-200516_create_transactions/down.sql
@@ -1,2 +1,2 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS token_balance;
+DROP TABLE IF EXISTS transactions;
--- crates/db/migrations/2025-08-29-041902_create_accounts/up.sql
+++ crates/db/migrations/2025-08-29-041902_create_accounts/up.sql
@@ -1,14 +1,14 @@
 -- Your SQL goes here
 CREATE TABLE accounts(
     id BIGSERIAL PRIMARY KEY,
-    block_id BIGINT NOT NULL REFERENCES blocks(id) ON DELETE CASCADE,
     slot BIGINT NOT NULL,
-    pubkey TEXT NOT NULL,
+    is_startup BOOLEAN NOT NULL,
+    pubkey BYTEA NOT NULL,
     lamports BIGINT NOT NULL,
-    owner TEXT NOT NULL,
+    owner BYTEA NOT NULL,
     executable BOOLEAN NOT NULL,
     rent_epoch BIGINT NOT NULL,
     data BYTEA NOT NULL,
     write_version BIGINT NOT NULL,
-    txn_signature TEXT
+    txn_signature BYTEA
 )
